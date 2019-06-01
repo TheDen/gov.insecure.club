5f779d59fc6b6106484a8eb89622a3032e67ec49
TheDen <deniskhoshaba@gmail.com>
2019-06-01 22:51:51 +1000

    cleanup script

--- generate-links.sh
+++ generate-links.sh
@@ -8,6 +8,5 @@ while IFS= read -r line; do
   govTitle="$(echo "${line}" | awk -F , '{print $2}' | sed -e 's/^"//' -e 's/"$//')"
   govURL="$(echo "${line}" | awk -F , '{print $1}' | sed -e 's/^"//' -e 's/"$//')"
   doc="<h3>${govTitle}</h3><h4><a target=\"_blank\" rel=\"noreferrer noopener\" href=\"${govURL}\">${govURL}</a><h4><hr>"
-  echo "$doc"
-
+  echo "${doc}"
 done < "${govCSV}"
